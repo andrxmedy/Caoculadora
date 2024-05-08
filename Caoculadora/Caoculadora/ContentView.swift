c0d85156d43c9c1a35e005c4a51d9093e0c60c95
Letícia Andrade <andradeleticia456@gmail.com>
2024-05-08 18:04:11 -0300

    Adiciona imagem e botao

--- Caoculadora/Caoculadora/ContentView.swift
+++ Caoculadora/Caoculadora/ContentView.swift
@@ -9,20 +9,49 @@ import SwiftUI
 
 struct ContentView: View {
     @State var years: Int? = nil
-    @State var month: Int? = nil
+    @State var months: Int? = nil
+    @State var result: Int?
+    
     var body: some View {
         VStack (alignment: .leading) {
         
-            Text("Qual é a idade do seu cao?")
+            Text("Qual é a idade do seu cão?")
+                .font(.system(size: 24))
             Text("Anos")
             TextField("Digite quantos anos seu cao tem:",
                       value: $years, format: .number)
             Text("Meses")
             TextField("Digite quantos meses além disso ele tem:",
-                      value: $month, format: .number)
+                      value: $months, format: .number)
+            Text("Porte")
+            // SEGMENTED CONTROL
+            if let result {
+                Text("Seu cachorro tem, em idade humana...")
+                Text("\(result) anos")
+            } else {
+                Image("clarinha")
+                                    .resizable()
+                                    .scaledToFit()
+                                    .frame(maxHeight: 150)
+                                    .frame(maxWidth: /*@START_MENU_TOKEN@*/.infinity/*@END_MENU_TOKEN@*/)// centralizou na tela
+                                .shadow(radius: 20)
+            }
+            Button(action: {
+                result = 23
+            }, label: {
+                ZStack{
+                    Color.indigo
+                    Text("Cãocular")
+                        .foregroundStyle(.white)
+                }
+            })
+            .cornerRadius(10)
+            .frame(height: 50)
+    
+            
             
         }
-        .textFieldStyle(.roundedBorder).keyboardType(.numberPad).bold().fontDesign(.rounded). padding()
+        .textFieldStyle(.roundedBorder).keyboardType(.numberPad).bold().fontDesign(.rounded).padding()
         .padding()
     }
 }
